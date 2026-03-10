87b5190b8387388cf3e1b29606a2015bd536d0ab
Anderson007a <androj888@gmail.com>
2026-03-09 22:08:50 -0500

    Adjuntamos lo de la clase

---
+++ cicloFor.py
@@ -0,0 +1,22 @@
+"""
+for x in range(1, 11):
+    print(x)
+"""
+"""
+for x in range(5):
+    print(x)
+"""
+"""
+for x in range(1, 11, 2):
+    print(x)
+"""
+"""
+texto = "Python"
+for letra in texto:
+    print(letra)
+"""
+tabla = int(input("Ingrese el numero de la tabla que desea: "))
+
+for x in range (1, 11):
+    print(tabla, " x ", x," = ", tabla * x)
+        
--- ciclo_while.py
+++ ciclo_while.py
@@ -1,29 +1,70 @@
 menu = """
     Restaurante el buen sabor
-    1. Hamburguesa
-    2. Pizza
-    3. Ensalada
-    4. Salchipapa
-    5. Perro Caliente
+    1. Hamburguesa - $20.000
+    2. Pizza - $7.500
+    3. Ensalada - $4.500
+    4. Salchipapa - $8000
+    5. Perro Caliente - $12.000
     6. Salir   
     """
+
     
+menu = """
+1. Hamburguesa ($20.000)
+2. Pizza ($7.500)
+3. Ensalada ($4.500)
+4. Salchipapa ($8.000)
+5. Perro Caliente ($12.000)
+6. Salir y Ver Factura
+"""
+
 print(menu)
+total_cuenta = 0
+cantidadHamburguesas = 0 
+cantidadPizza = 0
+cantidadEnsalada = 0 
+cantidadSalchipapa = 0
+cantidadPerro_Caliente = 0
+
 opcion = 0
 while opcion != 6:
-    opcion = int(input("Ingrese una opcion del menu "))
+    opcion = int(input("\nIngrese una opcion del menu: "))
     if opcion == 1:
-        print("Haz elegido hamburguesa")    
+        print("Has elegido hamburguesa")
+        total_cuenta += 20000
+        cantidadHamburguesas += 1     
     elif opcion == 2:
-        print("Haz elegido Pizza")    
+        print("Has elegido Pizza")
+        total_cuenta += 7500    
+        cantidadPizza += 1
     elif opcion == 3:
-        print("Haz elegido Ensalada")    
+        print("Has elegido Ensalada")
+        total_cuenta += 4500
+        cantidadEnsalada += 1    
     elif opcion == 4:
-        print("Haz elegido Salchipapa")    
+        print("Has elegido Salchipapa")
+        total_cuenta += 8000 
+        cantidadSalchipapa += 1   
     elif opcion == 5:
-        print("Haz elegido Perro Caliente")    
+        print("Has elegido Perro Caliente")
+        total_cuenta += 12000
+        cantidadPerro_Caliente += 1    
     elif opcion == 6: 
-        print("Gracias por visitar el restaurante el buen sabor hasta luego")
+        # Calculamos los totales antes de imprimir
+        totalHamburguesas = cantidadHamburguesas * 20000
+        totalPizza = cantidadPizza * 7500
+        totalEnsalada = cantidadEnsalada * 4500
+        totalSalchipapa = cantidadSalchipapa * 8000
+        totalPerro = cantidadPerro_Caliente * 12000
+        
+        print("\n--- FACTURA FINAL ---")
+        print(f"Hamburguesas: {cantidadHamburguesas} | Subtotal: ${totalHamburguesas}")
+        print(f"Pizzas:       {cantidadPizza} | Subtotal: ${totalPizza}")
+        print(f"Ensaladas:    {cantidadEnsalada} | Subtotal: ${totalEnsalada}")
+        print(f"Salchipapas:  {cantidadSalchipapa} | Subtotal: ${totalSalchipapa}")
+        print(f"Perros Cal.:  {cantidadPerro_Caliente} | Subtotal: ${totalPerro}")
+        print("---------------------")
+        print(f"TOTAL A PAGAR: ${total_cuenta}")
         break    
     else: 
         print("Opcion no valida en el menu")
