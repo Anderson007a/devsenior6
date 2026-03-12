912f187478d1c1a24486eeac84ce044f674aefe1
Anderson007a <androj888@gmail.com>
2026-03-11 20:16:54 -0500

    Retocando el ciclo for

--- cicloFor.py
+++ cicloFor.py
@@ -15,10 +15,13 @@ texto = "Python"
 for letra in texto:
     print(letra)
 """
+"""
 nombre = (input("Ingresa tu nombre: "))
 tabla = int(input(f"Ingrese el numero de la tabla que desea: "))
 
 print(nombre, f"Elegiste la tabla de multiplicar del {tabla}")
 for x in range (1, 11):
     print(f"{tabla} x {x:2} = {tabla * x:2}")
-        
+"""
+
+  
